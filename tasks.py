f07b4d3d8829ccb7483756bb2e50ebd6d1255de0
mprentice13 <matt.prentice@c2fo.com>
2026-04-13 12:09:41 -0500

    spring cleaning

--- tasks.py
+++ tasks.py
@@ -2,35 +2,6 @@ from invoke import task, UnexpectedExit
 import os
 import sys
 
-#------------------Brew Tasks------------------#
-
-@task
-def brew_manage(c):
-    "Managing Brew: checking for outdated packages, updating, and upgrading"
-    try:
-        c.run("brew outdated")
-        c.run("brew update")
-        c.run("brew upgrade")
-    except UnexpectedExit as e:
-        if "Cannot install terragrunt because conflicting formulae are installed" in e.result.stderr:
-            print("Conflicting formulae detected. Unlinking tgenv...")
-            c.run("brew unlink tgenv")
-            c.run("brew upgrade")
-        elif "Cannot install tgenv because conflicting formulae are installed" in e.result.stderr:
-            print("Conflicting formulae detected. Unlinking terragrunt...")
-            c.run("brew unlink terragrunt")
-            c.run("brew upgrade")
-        elif "Cannot install tfenv because conflicting formulae are installed" in e.result.stderr:
-            print("Conflicting formulae detected. Unlinking terraform...")
-            c.run("brew unlink terraform")
-            c.run("brew upgrade")
-        elif "Cannot install terraform because conflicting formulae are installed" in e.result.stderr:
-            print("Conflicting formulae detected. Unlinking tfenv...")
-            c.run("brew unlink tfenv")
-            c.run("brew upgrade")
-        else:
-            raise e
-
 #------------------Git Tasks------------------#
 
 @task
@@ -75,7 +46,7 @@ def git_push(c):
 @task
 def git_pr(c):
     "Create a new pull request"
-    c.run("gh pr create -d -f -a mprentice14 -r C@FO/devops,tbplayer7,jacob-m-barnes,MonaFraise,rontoc2,kuldeep14327,someshsharma21,mprentice14")
+    c.run("gh pr create -d -f -a mprentice14 -r C2FO/devops,tbplayer7,jacob-m-barnes,MonaFraise,rontoc2,kuldeep14327,someshsharma21,mprentice14")
 
 @task
 def git_manage(c):
